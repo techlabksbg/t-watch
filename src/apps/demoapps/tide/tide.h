ac56b048f6d35943b860c448980223fd06c5dd09
sebug <sebastian.gfeller@gmail.com>
2021-07-10 22:50:11 +0200

    Layout for low, high tide

--- src/apps/demoapps/tide/tide.h
+++ src/apps/demoapps/tide/tide.h
@@ -15,4 +15,10 @@ class Tide : public App {
     
     void* getIcon() { return (void*) &tideicon; }
     const char * getName() { return "Tide"; }
+
+    lv_obj_t* itemsPage;
+    lv_obj_t* highTitle;
+    lv_obj_t* highLabel;
+    lv_obj_t* lowTitle;
+    lv_obj_t* lowLabel;
 };
