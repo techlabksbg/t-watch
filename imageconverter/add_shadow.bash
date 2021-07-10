260be191a0b8d9bc70b3cd3b9da1630a45dd6fe8
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-07-10 20:30:23 +0200

    white shadow converter script

--- imageconverter/add_shadow.bash
+++ imageconverter/add_shadow.bash
@@ -4,11 +4,12 @@ LIC=abouticon.txt
 
 echo "Converting $PNG to $OUT"
 
-convert "$PNG" \( +clone -background white -shadow 70x3+1+1 \) +swap -background none -layers flatten +repage -crop 72x72+7+7 "$OUT"
+convert "$PNG" \( +clone -background white -shadow 70x3+1+1 \) +swap -background none -layers flatten +repage -crop 72x72+0+0 "$OUT"
 
 mv "$OUT" "$PNG"
 
 if [ -f $LIC ]
+then
 	(head -n 2 $LIC; echo "White shadow added by Ivo Blöchliger"; tail -n +2 $LIC) > temp24985u98e.txt
    mv temp24985u98e.txt $LIC
    echo Edited $LIC
