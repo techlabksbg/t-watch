f07f6ed968d70fbb65ec63ac316fc7a9175781ba
sebug <sebastian.gfeller@gmail.com>
2021-07-10 14:50:28 +0200

    Shopping list shows

--- src/apps/shoppinglist/shoppinglist.cpp
+++ src/apps/shoppinglist/shoppinglist.cpp
@@ -3,6 +3,14 @@
 bool ShoppingList::create() {
     register_for_swipe_up(myScr);
     lv_obj_add_style(myScr, LV_OBJ_PART_MAIN, &styles.background);
-    
+
+    Serial.println("Try log shop");
+
+    return true;
+}
+
+bool ShoppingList::show() {
+    Serial.println("Try show shop");
+
     return true;
 }
