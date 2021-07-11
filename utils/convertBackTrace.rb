8ee3c6eb0480589c9d718d7b456133971a22072c
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-07-11 11:29:22 +0200

    analyse backtrace

---
+++ utils/convertBackTrace.rb
@@ -0,0 +1,14 @@
+while line=gets
+  line.scan(/0x[0-9a-f]+:0x[0-9a-f]+/).each{|addr|
+    #p addr
+    cmd = "~/.platformio/packages/toolchain-xtensa32/bin/xtensa-esp32-elf-addr2line -pfiaC -e ../.pio/build/ttgo-t-watch/firmware.elf #{addr}"
+    res = `#{cmd}`#.gsub(/^.*?: (.*?) ..\/IvosWatch\//, "$1 ../")
+#    0x400fa17a: scrl_reposition at /home/ivo/burggraben/informatik/labor/arduino/t-watch/IvosWatch/.pio/libdeps/ttgo-t-watch/TTGO TWatch Library/src/lvgl/src/lv_widgets/lv_page.c:1172
+    puts res
+    res = res.scan(/^(0x[0-9a-f]+): (.*?) at (.*):([0-9]+)$/)
+    addr,func,file,line = *(res[0]) 
+    file.gsub!(/^.*IvosWatch/, "..")
+    puts "less -N +#{line} '#{file}'"
+  }
+end
+
