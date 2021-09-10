799e086d89b57d9d6e81e19e6531a8a190752da9
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-09-10 20:08:33 +0200

    ubuntumono fonts header with comments

---
+++ src/apps/styles/fonts/ubuntumono.h
@@ -0,0 +1,18 @@
+/**
+ * These fonts are intended for watches and only contain the symbols
+ 
+ 0123456789:.-
+
+ and are 4bits/pixel, made from 
+ /usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf
+ and
+ /usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf
+ 
+
+ Convert at https://lvgl.io/tools/fontconverter
+ 
+ */
+
+extern lv_font_t ubuntumonob32;
+extern lv_font_t ubuntumonor32;
+extern lv_font_t ubuntumonob64;
