ca1c6dfca5702a7952b47efe093ae693dc431f6f
PhilKu7 <ghphil@zab.li>
2021-09-24 18:08:16 +0200

    stopwatch working with wrong button labels

--- src/apps/demoapps/stopwatch/stopwatch.cpp
+++ src/apps/demoapps/stopwatch/stopwatch.cpp
@@ -4,9 +4,32 @@ bool Stopwatch::create()
 {
     register_for_swipe_up(myScr);
     lv_obj_add_style(myScr, LV_OBJ_PART_MAIN, &styles.background);
+
     stopwatch = lv_label_create(myScr, NULL);
+    // lv_obj_add_style(stopwatch, LV_OBJ_PART_MAIN, &styles.textLabel);
     lv_obj_add_style(stopwatch, LV_OBJ_PART_MAIN, &styles.titleLabel);
     lv_label_set_text(stopwatch, "Stopwatch");
+    lv_label_set_align(stopwatch, LV_LABEL_ALIGN_CENTER);
+    lv_obj_align(stopwatch, NULL, LV_ALIGN_IN_TOP_LEFT, 20, 20);
+
+    time_passed = lv_label_create(myScr, NULL);
+    lv_obj_add_style(time_passed, LV_OBJ_PART_MAIN, &styles.textLabel);
+    lv_label_set_text(time_passed, "0:00");
+    lv_label_set_align(time_passed, LV_LABEL_ALIGN_CENTER);
+    lv_obj_align(time_passed, NULL, LV_ALIGN_IN_TOP_LEFT, 20, 50);
+
+    start_stop_button = styles.stdButton(
+        myScr, "Start", [](lv_obj_t *button, lv_event_t event)
+        {
+            if (event != LV_EVENT_SHORT_CLICKED)
+                return;
+            ((Stopwatch *)(button->user_data))->start_stop_time();
+        },
+        this);
+    start_stop_label = lv_obj_get_child(start_stop_button, NULL);
+    lv_label_set_align(start_stop_label, LV_LABEL_ALIGN_CENTER);
+    // lv_label_set_text
+    lv_obj_align(start_stop_button, myScr, LV_ALIGN_IN_TOP_LEFT, 20, 75);
 
     Serial.println("Creating Stopwatch");
     return true;
@@ -15,5 +38,54 @@ bool Stopwatch::create()
 bool Stopwatch::show()
 {
     Serial.println("Showing Stopwatch");
+    loop();
+    start_loop(10);
     return true;
 }
+
+void Stopwatch::loop()
+{
+    time_t time_now;
+    struct tm info;
+    char buf[64];
+    time(&time_now);
+    time_now -= start_stop_t;
+    localtime_r(&time_now, &info);
+    strftime(buf, sizeof(buf), "%M:%S", &info);
+    if (running)
+    {
+        lv_label_set_text(time_passed, buf);
+    }
+}
+
+bool Stopwatch::hide()
+{
+    stop_loop();
+    return true;
+}
+
+void Stopwatch::start_stop_time()
+{
+    if (running)
+    {
+        lv_label_set_text(start_stop_label, "Start");
+    lv_label_set_align(start_stop_label, LV_LABEL_ALIGN_CENTER);
+
+        // lv_label_set_text(start_stop_button, "Start");
+        running = false;
+        return;
+    }
+    struct tm info;
+    time(&start_stop_t);
+    localtime_r(&start_stop_t, &info);
+    lv_label_set_text(start_stop_label, "Reset");
+    lv_label_set_align(start_stop_label, LV_LABEL_ALIGN_CENTER);
+
+    // lv_label_set_text(start_stop_button, "Stop");
+    running = true;
+}
+
+// bool Stopwatch::destroy()
+// {
+//     return true;
+// }
--- src/apps/demoapps/stopwatch/stopwatch.h
+++ src/apps/demoapps/stopwatch/stopwatch.h
@@ -9,10 +9,17 @@ class Stopwatch : public App
 
     virtual bool create();
     virtual bool show();
-    virtual bool hide() { return true; };
+    virtual bool hide(); // { return true; };
     virtual bool destroy() { return true; }
+
+    virtual void loop();
 
     void *getIcon() { return (void *)&stopwatchicon; }
     const char *getName() { return "Stopwatch"; }
-    lv_obj_t *stopwatch;
+
+private:
+    lv_obj_t *stopwatch, *time_passed, *start_stop_button, *start_stop_label;
+    time_t start_stop_t;
+    void start_stop_time();
+    bool running;
 };
