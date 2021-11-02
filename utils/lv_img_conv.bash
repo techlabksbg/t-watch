8fa6791422b9b4858eda3bc79492308325627077
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-11-02 14:27:53 +0100

    Online image conversion has changed.

--- utils/lv_img_conv.bash
+++ utils/lv_img_conv.bash
@@ -5,6 +5,12 @@ OUTC="${PNG%.*}.c"
 
 echo "Converting $PNG to $OUT"
 
-curl -F "img_file=@$PNG" -F "name=$OUT" -F cf=true_color_alpha -F format=c_array -F submit=Convert https://lvgl.io/tools/img_conv_core.php > $OUTC
+echo "The Webconverter has changed. Change this script or use the offline converter found here"
+echo "https://github.com/lvgl/lv_img_conv"
+echo
+
+echo "The online converter can be found here: https://lvgl.io/tools/imageconverter"
+
+# curl -F "img_file=@$PNG" -F "name=$OUT" -F cf=true_color_alpha -F format=c_array -F submit=Convert https://lvgl.io/tools/img_conv_core.php > $OUTC
 
 
