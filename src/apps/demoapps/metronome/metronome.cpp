935f439ad7340d8bc8e8591073ff9acdc1db68de
PhilKu7 <86799686+PhilKu7@users.noreply.github.com>
2021-11-12 17:22:41 +0100

    more or less working metronome beeps via serial print bpm not very exact

--- src/apps/demoapps/metronome/metronome.cpp
+++ src/apps/demoapps/metronome/metronome.cpp
@@ -1,3 +1,7 @@
+/**
+ * @author Philipp Kündig
+ */
+
 #include "metronome.h"
 
 bool Metronome::create()
@@ -5,148 +9,90 @@ bool Metronome::create()
     register_for_swipe_up(myScr);
     lv_obj_set_style_local_bg_color(myScr, LV_OBJ_PART_MAIN, LV_STATE_DEFAULT, LV_COLOR_BLACK);
 
-    stopwatch = lv_label_create(myScr, NULL);
-    lv_obj_add_style(stopwatch, LV_OBJ_PART_MAIN, &styles.titleLabel);
-    lv_label_set_text(stopwatch, "Stopwatch");
-    lv_label_set_align(stopwatch, LV_LABEL_ALIGN_CENTER);
-    lv_obj_align(stopwatch, NULL, LV_ALIGN_IN_TOP_LEFT, 20, 20);
+    slider = lv_slider_create(myScr, NULL);
+    lv_obj_set_width(slider, 200);
+    lv_obj_align(slider, myScr, LV_ALIGN_CENTER, 0, 0);
+    lv_slider_set_range(slider, 20, 250);
+    lv_slider_set_value(slider, 80, LV_ANIM_ON);
+    lv_obj_set_user_data(slider, this);
 
-    time_passed = lv_label_create(myScr, NULL);
-    lv_obj_add_style(time_passed, LV_OBJ_PART_MAIN, &styles.textLabel);
-    lv_label_set_text(time_passed, "00:00");
-    lv_label_set_align(time_passed, LV_LABEL_ALIGN_CENTER);
-    lv_obj_align(time_passed, NULL, LV_ALIGN_IN_TOP_LEFT, 20, 50);
+    metronome_speed_label = lv_label_create(myScr, NULL);
+    lv_obj_add_style(metronome_speed_label, LV_OBJ_PART_MAIN, &styles.textLabel);
+    set_labels(80);
 
-    last_time = lv_label_create(myScr, NULL);
-    lv_obj_add_style(last_time, LV_OBJ_PART_MAIN, &styles.textLabel);
-    lv_label_set_text(last_time, "00:00");
-    lv_label_set_align(last_time, LV_LABEL_ALIGN_CENTER);
-    lv_obj_align(last_time, NULL, LV_ALIGN_IN_TOP_LEFT, 150, 50);
+    metronome = lv_label_create(myScr, NULL);
+    lv_obj_add_style(metronome, LV_OBJ_PART_MAIN, &styles.titleLabel);
+    lv_label_set_text(metronome, "Metronome");
+    lv_label_set_align(metronome, LV_LABEL_ALIGN_CENTER);
+    lv_obj_align(metronome, NULL, LV_ALIGN_IN_TOP_LEFT, 20, 20);
 
-    start_pause_resume_button = styles.stdButton(
-        myScr, "Start", [](lv_obj_t *button, lv_event_t event)
-        {
-            if (event != LV_EVENT_SHORT_CLICKED)
-                return;
-            ((Metronome *)(button->user_data))->start_pause_resume_time();
-        },
-        this);
-    start_pause_resume_label = lv_obj_get_child(start_pause_resume_button, NULL);
-    lv_label_set_align(start_pause_resume_label, LV_LABEL_ALIGN_CENTER);
-    lv_obj_align(start_pause_resume_button, myScr, LV_ALIGN_IN_TOP_LEFT, 20, 75);
-
-    stop_reset_button = styles.stdButton(
-        myScr, "Start", [](lv_obj_t *button, lv_event_t event)
-        {
-            if (event != LV_EVENT_SHORT_CLICKED)
-                return;
-            ((Metronome *)(button->user_data))->stop_reset_time();
-        },
-        this);
-    stop_reset_label = lv_obj_get_child(stop_reset_button, NULL);
-    lv_label_set_align(stop_reset_label, LV_LABEL_ALIGN_CENTER);
-    lv_obj_align(stop_reset_button, myScr, LV_ALIGN_IN_TOP_LEFT, 20, 125);
-
-    Serial.println("Creating Stopwatch");
+    Serial.println("Creating Metronome");
     return true;
 }
 
 bool Metronome::show()
 {
-    Serial.println("Showing Stopwatch");
-    loop();
-    start_loop(10);
+    Serial.println("Showing Metronome");
+    // loop();
+    // start_loop(6/(80*100));
+    start_loop(60000/80);
+    lv_obj_set_event_cb(slider, slider_cb);
     return true;
 }
 
 void Metronome::loop()
 {
-    time_t time_now;
-    struct tm info;
-    char buf[64];
-    time(&time_now);
-    time_now -= start_stop_t;
-    localtime_r(&time_now, &info);
-    strftime(buf, sizeof(buf), "%M:%S", &info);
-    if (state == 0)
-    {
-        lv_label_set_text(time_passed, buf);
-    }
-    if (state != last_state)
-    {
-        switch (state)
-        {
-        case 0: // Running
-            if (last_state == 3)
-            {
-                time(&start_stop_t);               // reset time
-                localtime_r(&start_stop_t, &info); // reset time
-            }
-            lv_label_set_text(start_pause_resume_label, "Pause");
-            lv_label_set_text(stop_reset_label, "Stop");
-            break;
-        case 1: // Paused
-            lv_label_set_text(last_time, buf);
-            lv_label_set_text(start_pause_resume_label, "Resume");
-            lv_label_set_text(stop_reset_label, "Reset");
-            break;
-        case 2: // Stopped
-            lv_label_set_text(start_pause_resume_label, "Resume");
-            lv_label_set_text(stop_reset_label, "Reset");
-            break;
-        case 3: // Resetted
-            time_t reset_time = 0;
-            localtime_r(&reset_time, &info);
-            strftime(buf, sizeof(buf), "%M:%S", &info);
-            lv_label_set_text(time_passed, buf);
-            lv_label_set_text(last_time, buf);
-            lv_label_set_text(start_pause_resume_label, "Start");
-            lv_label_set_text(stop_reset_label, "----");
-            break;
-        }
-    }
-    last_state = state;
+    Serial.println("beep");
+    // time_t time_now;
+    // struct tm info;
+    // char buf[64];
+    // time(&time_now);
+    // time_now -= beg_t;
+    // localtime_r(&time_now, &info);
+    // strftime(buf, sizeof(buf), "%M:%S", &info);
+    
+    // if (state == 0)
+    // {
+    //     lv_label_set_text(time_passed, buf);
+    // }
+    // if (state != last_state)
+    // {
+    //     switch (state)
+    //     {
+    //     case 0: // Running
+    //         if (last_state == 3)
+    //         {
+    //             time(&start_stop_t);               // reset time
+    //             localtime_r(&start_stop_t, &info); // reset time
+    //         }
+    //         lv_label_set_text(start_pause_resume_label, "Pause");
+    //         lv_label_set_text(stop_reset_label, "Stop");
+    //         break;
+    //     case 1: // Paused
+    //         lv_label_set_text(last_time, buf);
+    //         lv_label_set_text(start_pause_resume_label, "Resume");
+    //         lv_label_set_text(stop_reset_label, "Reset");
+    //         break;
+    //     case 2: // Stopped
+    //         lv_label_set_text(start_pause_resume_label, "Resume");
+    //         lv_label_set_text(stop_reset_label, "Reset");
+    //         break;
+    //     case 3: // Resetted
+    //         time_t reset_time = 0;
+    //         localtime_r(&reset_time, &info);
+    //         strftime(buf, sizeof(buf), "%M:%S", &info);
+    //         lv_label_set_text(time_passed, buf);
+    //         lv_label_set_text(last_time, buf);
+    //         lv_label_set_text(start_pause_resume_label, "Start");
+    //         lv_label_set_text(stop_reset_label, "----");
+    //         break;
+    //     }
+    // }
+    // last_state = state;
 }
 
 bool Metronome::hide()
 {
     stop_loop();
     return true;
-}
-
-void Metronome::start_pause_resume_time()
-{
-    switch (state)
-    {
-    case 0: // resetted
-        state = 1;
-        break;
-    case 1: // running
-        state = 0;
-        break;
-    case 2: // paused
-        state = 0;
-        break;
-    case 3:
-        state = 0;
-        break;
-    }
-}
-
-void Metronome::stop_reset_time()
-{
-    switch (state)
-    {
-    case 0: // resetted (0)
-        state = 2;
-        break;
-    case 1: // running
-        state = 3;
-        break;
-    case 2: // paused
-        state = 3;
-        break;
-    case 3: // paused
-        break;
-    }
 }
--- src/apps/demoapps/metronome/metronome.h
+++ src/apps/demoapps/metronome/metronome.h
@@ -1,3 +1,7 @@
+/**
+ * @author Philipp Kündig
+ */
+
 #pragma once
 
 #include "../../app.h"
@@ -18,8 +22,26 @@ class Metronome : public App
     const char *getName() { return "Metronome"; }
 
 private:
-    lv_obj_t *stopwatch, *time_passed, *last_time, *start_pause_resume_button, *start_pause_resume_label, *stop_reset_button, *stop_reset_label;
-    time_t start_stop_t;
-    void start_pause_resume_time(), stop_reset_time();
-    int state = 3, last_state = 0;
+    lv_obj_t *metronome, *slider, *metronome_speed_label;
+    // time_t beg_t;
+    void set_labels(int speed)
+    {
+        static char buf[12];
+        snprintf(buf, 12, "%ubpm", speed);
+        lv_label_set_text(metronome_speed_label, buf);
+        stop_loop();
+        int wait_time = (60000/(float)speed)+0.5;
+        start_loop(wait_time);
+        // time(&beg_t);
+        lv_obj_align(metronome_speed_label, slider, LV_ALIGN_OUT_TOP_MID, 0, -30);
+    }
+    static void slider_cb(lv_obj_t *obj, lv_event_t event)
+    {
+        if (event == LV_EVENT_VALUE_CHANGED)
+        {
+            int speed_label = lv_slider_get_value(obj);
+            Metronome *speed = (Metronome *)lv_obj_get_user_data(obj);
+            speed->set_labels(speed_label);
+        }
+    }
 };
